39140204a37ab7eeebb81f3ebf99b688bd3d1037
Atl Cardoso <atl1cardoso0@gmail.com>
2024-12-18 23:31:17 -0600

    Se agregaron constantes de color y fondos necesarias

---
+++ lib/src/res/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+const Color backgroundColor2 = Color(0xFF17203A);
+const Color backgroundColorLight = Color(0xFFF2F6FF);
+const Color backgroundColorDark = Color(0xFF25254B);
+const Color shadowColorLight = Color(0xFF4A5367);
+const Color shadowColorDark = Colors.black;
+
+class AppColors {
+  AppColors._();
+
+  static const white = Color(0xffffffff);
+  static const blue = Color(0xff0043CE);
+  static const black = Color(0xff161616);
+  static const grey = Color(0xffC4C4C4);
+  static const lightScaffoldBg = Color(0xffE5E5E5);
+  static const darkScaffoldBg = Color(0xff333333);
+}
